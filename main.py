c3c4a20afea9c291b3a4ae1ad17f75b5d8e4e62f
Mutsnogkk <144063824+Mutsnogkk@users.noreply.github.com>
2026-05-20 12:08:31 -0300

    main.py

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ import os
 from datetime import datetime, timedelta
 
 # ================= CONFIG =================
-bot.run(os.getenv("TOKEN"))
 TICKET_CHANNEL_ID = 1412218164607057937
 TICKET_CATEGORY_ID = 1434521318744657950
 ARCHIVE_CATEGORY_ID = 1412218165017972752
 AVALIACAO_CHANNEL_ID = 1412218164607057934
-LOG_CHANNEL_ID = 1412218164774699127  # 🔥 LOGS AQUI
+LOG_CHANNEL_ID = 1412218164774699127
 STATUS_CHANNEL_ID = 1492005222967677070
-status_entregas = False  # começa OFF
 
-PIX_KEY = "f5e19e12-d8d9-4ead-8985-54da17dd140b"
+status_entregas = False
+PIX_KEY = "SUA_CHAVE_PIX_AQUI"
 
 MAX_TICKETS = 2
 SALES_FILE = "vendas.txt"
@@ -29,133 +28,12 @@ cancel_control = {}
 
 # ================= LOJA =================
 shop = {
-   
     "accension_5": {
         "nome": "🔮 Ascensão 1 até 5",
         "preco": "R$6,50",
         "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491942338480701540/content.png"
-    },
-    "accension_10": {
-        "nome": "🔮 Ascensão 5 até 10",
-        "preco": "R$15,50",
-        "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491942338480701540/content.png"
-    },
-    "aby_sigil": {
-        "nome": "🎀 (150x) Abyssal Sigil",
-        "preco": "R$3,50",
-        "img": "https://cdn.virtualities.com.br/catalog/product-f9ee454138ef7df52f2912fa09aa3a02.webp"
-    },
-    "Power_shard": {
-        "nome": "🎀 (150x) Power Shard",
-        "preco": "R$3,50",
-        "img": "https://cdn.virtualities.com.br/catalog/product-740ef37f9e1269518d0d79434991389d.webp"
-    },
-    "Frost_Relic": {
-        "nome": "🎀 (150x) Frost Relic",
-        "preco": "R$4,00",
-        "img": "https://cdn.discordapp.com/attachments/1339672109793935360/1491478635323068459/image.png"
-    },
-    "upper_seal": {
-        "nome": "🎀 (150x) Upper Seal",
-        "preco": "R$5,50",
-        "img": "https://cdn.virtualities.com.br/catalog/product-e016fee17e9a38ea3e3829b2eb436a04.webp"
-    },
-    "Broken_sword": {
-        "nome": "🎀 (150x) Broken Swords",
-        "preco": "R$3,50",
-        "img": "https://cdn.virtualities.com.br/catalog/product-d2e038a4c1bb8bd3c24185d0c75d8a3b.webp"
-    },
-    "reroll_clan": {
-        "nome": "🔄 (100x) Clan Reroll",
-        "preco": "R$1,50",
-        "img": "https://cdn.virtualities.shop/catalog/product-16297cc6201266ea7476b4c041a9cfe5.webp"
-    },
-    "reroll_race": {
-        "nome": "🧬 (1000x) Race Reroll",
-        "preco": "R$1,15",
-        "img": "https://cdn.virtualities.shop/catalog/product-b109b4ac544fb6d96cbd1327207d114f.webp"
-    },
-    "divine_grail": {
-        "nome": "🎀 (155x) Divine Grail",
-        "preco": "R$10,50",
-        "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491951362089418853/image.png"
-    },
-    "tempest_relic": {
-        "nome": "🎀 (155x) Tempest Relic",
-        "preco": "R$4,50",
-        "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491951361804341320/image.png"
-    },
-    "slime_key": {
-        "nome": "🎀 (155x) Slime Key",
-        "preco": "R$5,50",
-        "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491951361544425572/image.png"
-    },
-
-    "madara_Iceqqueen": {
-        "nome": "💥(Especial da Update) Set da Ice Queen",
-        "preco": "R$13,50",
-        "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491908057137938473/image.png"
-    },
-    "Set_madara": {
-        "nome": "🔥 Set Madara",
-        "preco": "R$10,20",
-        "img": "https://cdn.discordapp.com/attachments/1412218164607057939/1491480544695947388/image.png"
-    },
-    "set_guilga": {
-        "nome": "🔥 Set Completo Guilga",
-        "preco": "R$6,50",
-        "img": "https://cdn.virtualities.com.br/catalog/product-7a05145c80de5d4607aee7a215e634ea.png"
-    },
-    "f_guilga": {
-        "nome": "🔥 F do Guilga",
-        "preco": "R$6,50",
-        "img": "https://cdn.discordapp.com/attachments/1339672109793935360/1491213973385773187/image.png"
-    },
-    "set_atomic": {
-        "nome": "🔮 Set Atomic",
-        "preco": "R$8,90",
-        "img": "https://cdn.discordapp.com/attachments/1412218164607057939/1491463841240977640/image.png"
-    },
-    "set_saber": {
-        "nome": "⚔️ Set Completo Saber",
-        "preco": "R$6,50",
-        "img": "https://cdn.virtualities.shop/catalog/product-85946a787ed0f93222826dbf55619d70.png"
-    },
-    "set_rimuru": {
-        "nome": "❄️ Set Rimuru",
-        "preco": "R$6,50",
-        "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491951361309278308/image.png"
-    },
-    "set_anos": {
-        "nome": "🔥 Set Demon King",
-        "preco": "R$6,50",
-        "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491951361015681266/image.png"
-    },
-    "set_blessed": {
-        "nome": "✨ Set Blessed Maiden",
-        "preco": "R$6,50",
-        "img": "https://cdn.discordapp.com/attachments/1449247545728696576/1491951843125887068/image.png"
-    },
-    
-    
-
-    "aura": {
-        "nome": "✨ (1x) Aura Crate",
-        "preco": "R$2,20",
-        "img": "https://cdn.virtualities.shop/catalog/product-5f24a3325a89f08f17110ef25944d3db.webp"
-    },
-    "cosmetic": {
-        "nome": "🎁 (1x) Cosmetic Crate",
-        "preco": "R$2,20",
-        "img": "https://cdn.virtualities.shop/catalog/product-de811a89462c287485003289c18d1db2.webp"
-    },
-    "100_baumitico": {
-        "nome": "🎁 (100x) Baú Mítico",
-        "preco": "R$3,20",
-        "img": "https://cdn.virtualities.shop/catalog/product-c0ceab3d0461cd9d428bece5208937ab.webp"
     }
 }
-
 
 # ================= MENU =================
 class ShopSelect(Select):
@@ -189,13 +67,10 @@ class ShopSelect(Select):
 
         item = shop[self.values[0]]
 
-        print(f"[ABERTO] {interaction.user} abriu ticket | Item: {item['nome']}")
-
-        # 🔥 LOG ABERTURA
         log_channel = bot.get_channel(LOG_CHANNEL_ID)
         if log_channel:
             await log_channel.send(
-                f"🟢 **Ticket Aberto**\n👤 {interaction.user.mention}\n📦 {item['nome']}"
+                f"🟢 Ticket Aberto\n👤 {interaction.user.mention}\n📦 {item['nome']}"
             )
 
         overwrites = {
@@ -214,11 +89,6 @@ class ShopSelect(Select):
         embed.add_field(name="🛒 Item", value=item["nome"], inline=False)
         embed.add_field(name="💰 Preço", value=item["preco"], inline=True)
         embed.add_field(name="💳 Pix", value=PIX_KEY, inline=False)
-        embed.add_field(
-            name="⚠️ Importante",
-            value="Envie o comprovante.\n\nEntrega apenas em servidor privado.\n\n⭐ Avalie em <#1412218164607057934>",
-            inline=False
-        )
 
         view = View(timeout=None)
 
@@ -226,15 +96,6 @@ class ShopSelect(Select):
             if not i.user.guild_permissions.administrator:
                 await i.response.send_message("Só ADM pode finalizar.", ephemeral=True)
                 return
-
-            print(f"[FINALIZADO] {i.user} finalizou ticket de {interaction.user}")
-
-            # 🔥 LOG FINALIZADO
-            log_channel = bot.get_channel(LOG_CHANNEL_ID)
-            if log_channel:
-                await log_channel.send(
-                    f"🟡 **Pedido Finalizado**\n👤 Cliente: {interaction.user.mention}\n🛠 Staff: {i.user.mention}\n📦 {item['nome']}"
-                )
 
             archive_category = discord.utils.get(guild.categories, id=ARCHIVE_CATEGORY_ID)
 
@@ -244,27 +105,6 @@ class ShopSelect(Select):
             await i.response.send_message("Pedido finalizado!")
 
         async def cancelar(i):
-            now = datetime.now()
-            user_id = i.user.id
-
-            cancel_control.setdefault(user_id, [])
-            cancel_control[user_id] = [t for t in cancel_control[user_id] if now - t < timedelta(minutes=20)]
-
-            if len(cancel_control[user_id]) >= 2:
-                await i.response.send_message("Limite de cancelamento.", ephemeral=True)
-                return
-
-            cancel_control[user_id].append(now)
-
-            print(f"[CANCELADO] {i.user} cancelou ticket")
-
-            # 🔥 LOG CANCELADO
-            log_channel = bot.get_channel(LOG_CHANNEL_ID)
-            if log_channel:
-                await log_channel.send(
-                    f"🔴 **Pedido Cancelado**\n👤 {i.user.mention}\n📦 {item['nome']}"
-                )
-
             await i.response.send_message("Cancelado.", ephemeral=True)
             await channel.delete()
 
@@ -286,22 +126,6 @@ class ShopView(View):
         self.add_item(ShopSelect())
 
 # ================= EVENTOS =================
-
-@bot.check
-async def only_admins(ctx):
-    # bloqueia DM também
-    if ctx.guild is None:
-        return False
-
-    # permite só ADM
-    return ctx.author.guild_permissions.administrator
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CheckFailure):
-        await ctx.send("❌ Só administradores podem usar comandos.")
-        
-        print(f"[BLOQUEADO] {ctx.author} ({ctx.author.id}) tentou usar comando em {ctx.channel}")
 @bot.event
 async def on_ready():
     print(f"Logado como {bot.user}")
@@ -317,53 +141,12 @@ async def on_ready():
             color=0x6f00ff
         )
 
-        file = discord.File("vabber.png", filename="vabber.png")
-        embed.set_image(url="attachment://vabber.png")
-        embed.set_footer(text="Atendimento rápido ⚡")
-
-        await channel.send(embed=embed, view=ShopView(), file=file)
-
-# ❤️ AUTO REAÇÃO NO CANAL DE AVALIAÇÃO
-@bot.event
-async def on_message(message):
-    if message.channel.id == AVALIACAO_CHANNEL_ID and not message.author.bot:
-        try:
-            await message.add_reaction("❤️")
-        except:
-            pass
-
-    await bot.process_commands(message)
-
-@bot.command()
-async def turnon(ctx):
-    global status_entregas
-
-    status_entregas = True
-    channel = bot.get_channel(STATUS_CHANNEL_ID)
-
-    if channel:
-        await channel.edit(name="🚨 • Entregas: ON")
-
-    await ctx.send("🚨 Status On")
-
-
-@bot.command()
-async def turnoff(ctx):
-    global status_entregas
-
-    status_entregas = False
-    channel = bot.get_channel(STATUS_CHANNEL_ID)
-
-    if channel:
-        await channel.edit(name="🚨 • Entregas: OFF")
-
-    await ctx.send("🚨 Status Off")
-
-# ================= COMANDOS =================
-@bot.command()
-async def status(ctx):
-    await ctx.send("🤖 Online!")
+        await channel.send(embed=embed, view=ShopView())
 
 # ================= START =================
+TOKEN = os.getenv("TOKEN")
 
-
+if not TOKEN:
+    print("ERRO: TOKEN não encontrado!")
+else:
+    bot.run(TOKEN)
